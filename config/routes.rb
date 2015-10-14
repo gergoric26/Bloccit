64525662cf30b78a033cc8dec1c4b278bbcc3d48
YOUR NAME <gergoric_dean@yahoo.com>
2015-10-13 23:30:50 -0400

    adding comment to posts

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,10 @@ Bloccit::Application.routes.draw do
     resources :sponsored_posts, except: [:index]
   end
 
+  resources :posts, only: [] do
+    resources :comments, only: [:create, :destroy]
+  end
+
   resources :users, only: [:new, :create]
 
   resources :sessions, only: [:new, :create, :destroy]
