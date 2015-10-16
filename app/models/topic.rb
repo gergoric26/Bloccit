63a2a582b3d71be63fb969f3f96f1407e5484c60
YOUR NAME <gergoric_dean@yahoo.com>
2015-10-16 11:11:17 -0400

    PrivateTopics

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -7,4 +7,8 @@ class Topic < ActiveRecord::Base
 
   validates :name, length: { minimum: 5 }, presence: true
   validates :description, length: { minimum: 15 }, presence: true
+
+  Topic.all.select(&:public)
+  
+  scope :visible_to, -> (user) { user ? all : where(public: true) }
 end
