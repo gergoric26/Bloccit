5b317be6558ba6c78d656380fce9e585326ba967
YOUR NAME <gergoric_dean@yahoo.com>
2015-10-17 14:21:02 -0400

    API-RetrievingData

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151015143636) do
+ActiveRecord::Schema.define(version: 20151016181210) do
 
   create_table "advertisements", force: :cascade do |t|
     t.string   "title"
@@ -148,6 +148,7 @@ ActiveRecord::Schema.define(version: 20151015143636) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.integer  "role"
+    t.string   "auth_token"
   end
 
   create_table "votes", force: :cascade do |t|
